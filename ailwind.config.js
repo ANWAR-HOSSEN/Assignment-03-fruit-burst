08ace23ad5ea3710dfb5a0b15c9cc42656ca3b86
ANWAR-HOSSEN <anwar21091@gmail.com>
2023-09-24 06:13:31 +0600

    fruit burst major partt

---
+++ ailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    theme: {
+      extend: {
+        backgroundImage: {
+          'hero-pattern': "url('/img/hero-pattern.svg')",
+          'footer-texture': "url('/img/footer-texture.png')",
+        }
+      }
+    }
+  }
+
+  module.exports = {
+    theme: {
+      extend: {
+        space: {
+          '5px': '5px',
+        }
+      }
+    }
+  }
